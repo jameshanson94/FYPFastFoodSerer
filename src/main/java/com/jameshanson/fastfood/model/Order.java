132ab1b32da7bef186034c1aa7c98a1f9286c417
jameshanson94 <jameshanson94@hotmail.co.uk>
2016-02-22 13:12:59 +0000

    updates order class to now have a collection of products instead of items

--- src/main/java/com/jameshanson/fastfood/model/Order.java
+++ src/main/java/com/jameshanson/fastfood/model/Order.java
@@ -23,7 +23,7 @@ public class Order {
     private OrderState orderState;
     private Date timeStamp;
     private long totalCost;
-    private List<Product> allItems;
+    private List<Product> allProducts;
     
     
     public Order() {
@@ -37,7 +37,7 @@ public class Order {
         this.orderState = new OrderedOrderState();
         this.timeStamp = timeStamp;
         this.totalCost = totalCost;
-        allItems = new ArrayList<>();
+        allProducts = new ArrayList<>();
     }
 
     public int getOrderId() {
@@ -88,16 +88,16 @@ public class Order {
         this.totalCost = totalCost;
     }
     
-    public void addItem(Product item) {
-        allItems.add(item);
+    public void addProduct(Product product) {
+        allProducts.add(product);
     }
     
-    public void remoteItem(Product item) {
-        allItems.remove(item);
+    public void remoteProduct(Product product) {
+        allProducts.remove(product);
     }
     
-    public List<Product> getItems() {
-        return allItems;
+    public List<Product> getProducts() {
+        return allProducts;
     }
 
     @Override
